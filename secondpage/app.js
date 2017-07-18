ee45db4e05cadea6356c0f76b9fc160b0aa6a8fe
sagarrr <biswas.sagar97@gmail.com>
2017-07-18 13:44:57 +0530

    second page

---
+++ secondpage/app.js
@@ -0,0 +1,42 @@
+var obj = {
+  "cityname":"New Delhi",
+   "citydescr":" Delhi is the capital city of India and is regarded as the heart of the nation. The city is popular for its enriched culture and heritage. The city hosts some famous historical monuments and is developing with the passing of time. The influence of religious diversity can be seen in the city along with the cultural impact of the Mughal, the ancient Indian and the British. There are many beautiful gardens in the city, away from pollution and busy city life that provide opportunities to walk leisurely in the midst of greenery. The capital city is divided into two sections popularly known as Purani Dilli or Old Delhi and Nayi Dilli or New Delhi. Old Delhi is popular for its ancient culture and monuments along with its overcrowded gastronomical lanes  ",
+   "lat":"28.6139° N",
+   "long":"77.2090° E",
+   "placetosee": [{
+     "name" : "India Gate",
+     "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/India_Gate_in_New_Delhi_03-2016.jpg/800px-India_Gate_in_New_Delhi_03-2016.jpg",
+     "descr" : "The India Gate, (originally called the All India War Memorial), is a war memorial located astride the Rajpath, on the eastern edge of the ceremonial axis of New Delhi, India, formerly called Kingsway. India Gate is a memorial to 82,000 soldiers of the Indian Army who died in the period 1914 to 21 in the First World War.",
+    },
+    {
+      "name" : "lotus temple",
+      "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Lotus_Temple-Panoroma-Visit_During_WCI_2016-_IMG_6471.jpg/240px-Lotus_Temple-Panoroma-Visit_During_WCI_2016-_IMG_6471.jpg",
+      "descr" : "The Lotus Temple, located in Delhi, India, is a Bahá'í House of Worship completed in 1986. Notable for its flowerlike shape, it has become a prominent attraction in the city. Like all Bahá'í Houses of Worship, the Lotus Temple is open to all, regardless of religion or any other qualification.",
+    },
+    {
+      "name" : "Humayun's Tomb",
+      "img" : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Humayun%27s_Tomb_from_the_Charbagh_-_1.jpg/800px-Humayun%27s_Tomb_from_the_Charbagh_-_1.jpg",
+      "descr" : "Humayun's tomb (Maqbara e Humayun) is the tomb of the Mughal Emperor Humayun in Delhi, India. The tomb was commissioned by Humayun's first wife and chief consort, Empress Bega Begum (also known as Haji Begum),[2][3][4][5][6][7][8] in 1569-70, and designed by Mirak Mirza Ghiyas, a Persian architect chosen by her."
+
+    }
+  ],
+   "hotels": [{
+     "type":"5-star hotel",
+     "name":"The Leela Palace",
+     "lat":"28.5800° N",
+     "long":"77.1890° E",
+     "img":"",
+     "address":"Africa Avenue, Chanakyapuri, Diplomatic Enclave, New Delhi, Delhi 110023, India",
+     "rating":"4.6",
+   },
+   {
+     "type":"budget hotel",
+     "name":"Hotel Le Roi",
+     "lat":"28.6442° N",
+     "long":"77.2106° E",
+     "img":"",
+     "address":"No.2206, Rajguru Road, Near New Delhi Railway Station, Chuna Mandi, Paharganj, Delhi, 110055, India",
+     "rating":"3.7"
+   }
+ ],
+ }
